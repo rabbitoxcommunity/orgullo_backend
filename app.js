07c05a1aa53a3b320b0cec10f6922708a5c2d5a1
aslam <aslam@smarthatch.com>
2024-12-29 08:36:48 +0530

    100 mb fixed

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "100mb" })); 
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
