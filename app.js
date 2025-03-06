9b5181f24888bffb19a73131fbc0947ac267f3c5
aslam <aslam@smarthatch.com>
2025-03-06 09:43:09 +0530

    changes

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 
 var app = express();
-dotenv.config()
+dotenv.config();
 
-//connect database 
-const connectDB = require('./utils/db')
+// Connect to database 
+const connectDB = require('./utils/db');
 connectDB();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,8 +24,7 @@ app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-const whitelist = JSON.parse(process.env.WHITE_LIST ?? "[]")
+const whitelist = JSON.parse(process.env.WHITE_LIST ?? "[]");
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -44,20 +43,23 @@ app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// **Add this part to start the server**
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
